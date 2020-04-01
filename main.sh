dc87ef36f5dcd344b3dfd4723d104d63e9d05752
rbjornstad <roger.bjornstad@nav.no>
2020-04-01 12:53:44 +0200

    Adding functionality for custom ports for each ns

--- main.sh
+++ main.sh
@@ -5,7 +5,9 @@ while true; do
   for ns in $(kubectl get namespace -o jsonpath="{ range .items[?(.metadata.annotations['rbac-sync\.nais\.io/group-name'])] }{.metadata.name } { end }"); do
     ips=$(kubectl get po -n istio-system -l app=prometheus -o json | jq -r -c '[.items[].status.podIP]')
     f=$(mktemp)
-    echo $ns
+    ports=$(kubectl get app -n $ns -o json | jq -r -c '.items[].spec.port' | grep -v null | sort -u)
+    echo "----- $ns -----"
+
 cat > "$f" <<EOF 
 apiVersion: security.istio.io/v1beta1
 kind: AuthorizationPolicy
@@ -20,12 +22,15 @@ spec:
     to:
     - operation:
         ports:
-        - "15090"
-        - "8080"
-        - "9090"
-        - "4242"
-  selector: {}
 EOF
+    for port in $ports; do
+      echo $port | awk '{ printf("        - \"%s\"\n", $1)}' >> $f
+    done
+    echo "  selector: {}" >> $f
+    
+    if [ ${#ports} -le 2 ]; then
+      continue
+    fi
     kubectl apply -f "$f"
   done
   sleep 60
