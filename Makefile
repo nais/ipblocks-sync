28c15cdd1b3e975eadcaeda620282f45f0a7071a
rbjornstad <roger.bjornstad@nav.no>
2020-04-01 13:38:12 +0200

    Adding envoy port

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-image=docker.pkg.github.com/nais/ipblocks-sync/ipblocks-sync:0.8
+image=docker.pkg.github.com/nais/ipblocks-sync/ipblocks-sync:0.12
 
 build:
 	docker build -t ${image} .
--- main.sh
+++ main.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 set -e
+
+sleep_time=60
+
 while true; do
   date
+  ips=$(kubectl get po -n istio-system -l app=prometheus -o json | jq -r -c '[.items[].status.podIP]')
   for ns in $(kubectl get namespace -o jsonpath="{ range .items[?(.metadata.annotations['rbac-sync\.nais\.io/group-name'])] }{.metadata.name } { end }"); do
-    ips=$(kubectl get po -n istio-system -l app=prometheus -o json | jq -r -c '[.items[].status.podIP]')
-    f=$(mktemp)
-    ports=$(kubectl get app -n $ns -o json | jq -r -c '.items[].spec.port' | grep -v null | sort -u)
     echo "----- $ns -----"
-
+    ports=$(kubectl get app -n "$ns" -o json | jq -r -c '.items[].spec.port' | grep -v null | sort -u)
+    if [ ${#ports} -le 2 ]; then
+      echo "No applications configured in namespace [$ns]"
+      continue
+    fi
+    f=$(mktemp)
 cat > "$f" <<EOF 
 apiVersion: security.istio.io/v1beta1
 kind: AuthorizationPolicy
@@ -22,16 +28,15 @@ spec:
     to:
     - operation:
         ports:
+        - "15090"
 EOF
     for port in $ports; do
-      echo $port | awk '{ printf("        - \"%s\"\n", $1)}' >> $f
+      echo "$port" | awk '{ printf("        - \"%s\"\n", $1)}' >> "$f"
     done
-    echo "  selector: {}" >> $f
-    
-    if [ ${#ports} -le 2 ]; then
-      continue
-    fi
+    echo "  selector: {}" >> "$f"
     kubectl apply -f "$f"
   done
-  sleep 60
+
+  echo "Sleeping for $sleep_time secs."
+  sleep $sleep_time
 done
