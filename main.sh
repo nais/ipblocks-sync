1c1a58d0c1d1fcca9b2ec2271ef7e3dc32937435
rbjornstad <roger.bjornstad@nav.no>
2020-04-02 08:43:54 +0200

    Refactoring of main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,26 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-sleep_time=60
+readonly sleep_time=60
 
 while true; do
   date
   ips=$(kubectl get po -n istio-system -l app=prometheus -o json | jq -r -c '[.items[].status.podIP]')
   for ns in $(kubectl get namespace -o jsonpath="{ range .items[?(.metadata.annotations['rbac-sync\.nais\.io/group-name'])] }{.metadata.name } { end }"); do
-    echo "----- $ns -----"
-    ports=$(kubectl get app -n "$ns" -o json | jq -r -c '.items[].spec.port' | grep -v null | sort -u)
-    if [ ${#ports} -le 1 ]; then
-      echo "No applications configured in namespace [$ns]"
+    declare -a PORTS=()
+    for json in $(kubectl get deploy -n "$ns" -o json | jq -r -c '.items[].spec.template.metadata.annotations'); do
+      scraping="$(echo "$json" | jq -r -c '."prometheus.io/scrape" == "true"')"
+      if $scraping; then
+        if [[ ! " ${PORTS[*]} " =~ $(echo "$json" | jq -r '."prometheus.io/port"') ]]; then
+          PORTS+=("$(echo "$json" | jq -r '."prometheus.io/port"')")
+        fi
+      fi
+
+    done
+    if [ ${#PORTS[@]} -eq 0 ]; then
       continue
     fi
     f=$(mktemp)
-cat > "$f" <<EOF 
+		cat > "$f" <<EOF 
 apiVersion: security.istio.io/v1beta1
 kind: AuthorizationPolicy
 metadata:
@@ -30,13 +36,17 @@ spec:
         ports:
         - "15090"
 EOF
-    for port in $ports; do
+    for port in "${PORTS[@]}"; do
       echo "$port" | awk '{ printf("        - \"%s\"\n", $1)}' >> "$f"
     done
     echo "  selector: {}" >> "$f"
-    kubectl apply -f "$f"
+    #kubectl apply -f "$f"
+    echo "Found deployments with prometheus scraping true in namespace $ns."
+		cat "$f"
   done
 
   echo "Sleeping for $sleep_time secs."
   sleep $sleep_time
+  break
 done
+
